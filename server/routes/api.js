fb2f44356fe21d540f963ba1485fe5b2320c23f1
didietsuryadi <didietsuryadi>
2017-03-21 12:10:39 +0700

    create-route

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,9 +1,19 @@
 var express = require('express');
 var router = express.Router();
-
+var controller = require('../controllers/houseController')
 /* GET users listing. */
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+router.get('/house', controller.getHouses);
+
+router.get('/house/:id', controller.getHouse);
+
+router.post('/house', controller.newHouses);
+
+router.put('/house/:id', controller.updateHouse);
+
+router.delete('/house/:id', controller.deleteHouse);
 
 module.exports = router;
